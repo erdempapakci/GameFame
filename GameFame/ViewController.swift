2121c621a505206e5927a7ac3bd23f5d412e8d8b
erdempapakci <erdmppkc@gmail.com>
2022-10-04 18:48:09 +0300

    App Init and UI Designs

---
+++ GameFame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameFame
+//
+//  Created by Erdem Papakçı on 4.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
