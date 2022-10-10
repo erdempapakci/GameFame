48f929a604245e716bb11e8accac57d6fcb1de87
erdempapakci <erdmppkc@gmail.com>
2022-10-10 17:32:33 +0300

    ScreenShots, Stores Movies endpoints added

---
+++ GameFame/Network/EndPoints.swift
@@ -0,0 +1,14 @@
+//
+//  EndPoints.swift
+//  GameFame
+//
+//  Created by Erdem Papakçı on 9.10.2022.
+//
+
+import Foundation
+
+enum APIEndpoints: String {
+    case screenshots = "screenshots"
+    case stores = "stores"
+    case movies = "movies"
+}
