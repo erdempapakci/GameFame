51884570e7aae4edd920da22c8a9c4b653aceb03
erdempapakci <erdmppkc@gmail.com>
2022-10-27 22:20:00 +0300

    Onboarding Scene is showing for once

--- GameFame/SceneDelegate.swift
+++ GameFame/SceneDelegate.swift
@@ -12,18 +12,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     private let storyboardName = "Main"
     private let tabbarControllerIdentifier = "TabBarController"
-
+    let vc : OnboardingViewController = OnboardingViewController()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+     
+        
+        if !UserDefaults.standard.bool(forKey: "didLoadedd") {
+            UserDefaults.standard.set(true, forKey: "didLoadedd")
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController")
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+        } else {
+            
+            let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+            let tabbarController = storyboard.instantiateViewController(withIdentifier: tabbarControllerIdentifier)
+            let navigationController = UINavigationController(rootViewController: tabbarController)
+            
+            guard let scene = (scene as? UIWindowScene) else { return }
+            
+            window = UIWindow(windowScene: scene)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+            
+        }
+       
       
-        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
-        let tabbarController = storyboard.instantiateViewController(withIdentifier: tabbarControllerIdentifier)
-        let navigationController = UINavigationController(rootViewController: tabbarController)
-        
-        guard let scene = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(windowScene: scene)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+       
      
     }
 
