fbdcb73f007c552c43f3bc8919bc6f32bbb71975
erdempapakci <erdmppkc@gmail.com>
2022-11-01 19:04:34 +0300

    API Constants Date from Date now extension, not directly to String

--- GameFame/Network/APIConstants.swift
+++ GameFame/Network/APIConstants.swift
@@ -10,9 +10,12 @@ import Foundation
 struct APIConstants {
     static let API_KEY = "API_KEY"
     static let BASE_URL = "https://api.rawg.io/api"
-    
-    static let METACRITIC_URL = "\(BASE_URL)/games?key=\(API_KEY)&dates=2000-01-01,2022-12-31&ordering=-metacritic&page_size=50"
-    static let POPULAR_URL = "\(BASE_URL)/games?key=\(API_KEY)&dates=2022-01-01,2022-12-31&ordering=-added"
-    static let UPCOMING_URL = "\(BASE_URL)/games?key=\(API_KEY)&dates=2022-09-28,2025-12-31&ordering=-added&page_size=10"
+    static var date = Date()
+    static var nowDate = date.getFormattedDate(format: "yyyy-MM-dd")
+   
+    static let METACRITIC_URL = "\(BASE_URL)/games?key=\(API_KEY)&dates=2000-01-01,\(nowDate)&ordering=-metacritic&page_size=50"
+    static let POPULAR_URL = "\(BASE_URL)/games?key=\(API_KEY)&dates=2022-01-01,\(nowDate)&ordering=-added"
+    static let UPCOMING_URL = "\(BASE_URL)/games?key=\(API_KEY)&dates=2022-09-28,\(nowDate)&ordering=-added&page_size=10"
     static let DISCOVER_URL = "\(BASE_URL)/games?key=\(API_KEY)&ordering=-added&page_size=40"
 }
+
